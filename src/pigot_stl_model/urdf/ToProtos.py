7bbe628dd73ec1133956027b8d67d8b793fbbc4e
KKXiaoKang <410080663@qq.com>
2024-06-02 23:15:49 -0700

    feat:增加四足机器人的模型文件功能包

---
+++ src/pigot_stl_model/urdf/ToProtos.py
@@ -0,0 +1,15 @@
+"""
+
+python -m urdf2webots.importer --input .\pigot_stl_model.urdf
+
+在webots文件夹自己的项目下，打开cmd命令行下输入该指令，即可将urdf生产pro文件导入到webots当中
+
+
+输出日志：
+
+Robot name: pigot_stl_model
+Directory "PigotStlModel_textures" already exists!
+Root link: world_link
+There are 14 links, 13 joints and 0 sensors
+
+"""
